35eb34f58d2cc09c6d4a20c205ebabce4d2b8b41
salman <salmanbinsalahudeen@gmail.com>
2024-09-14 13:07:34 +0530

    (property-card): fixde expanded styling

--- app/components/property-card.tsx
+++ app/components/property-card.tsx
@@ -1,63 +1,86 @@
- 
 import Image from "next/image";
-import assets from "@/app/assets/images/assetz.png";
+import image from "@/app/assets/images/image.png";
 import { useState } from "react";
 
 export default function PropertyCard() {
-  const [expandedState, setExpandedState] = useState<boolean>(false)
+  const [expandedState, setExpandedState] = useState<boolean>(false);
 
   const onClick = () => {
-    setExpandedState((prev) => !prev)
-  }
-
+    setExpandedState((prev) => !prev);
+  };
 
   return (
     <div className="bg-white">
-    <div className="p-4 px-8 flex w-full justify-between items-center bg-white gap-4 text-black h-[180px]">
-      <Image alt="assests" src={assets} height={143} width={143} className="bg-red-500" />
-      <div className="flex flex-col justify-start w-full gap-4">
-        <p className="text-[20px]">Apartments</p>
-        <p className="text-[32px] font-medium">Sobha </p>
-        <p className="text-[18px]">123</p>
-      </div>
-      <div className="flex flex-col h-full justify-between w-fit">
-        <div>
-          <p>HSR Layout</p>
-          <p> North Bangalore</p>
+      <div className="p-4 px-8 flex w-full justify-between items-center bg-white gap-4 text-black h-[180px]">
+        <Image
+          alt="assests"
+          src={image}
+          height={143}
+          width={143}
+          className="h-[143px] w-[143px]"
+        />
+        <div className="flex flex-col justify-start w-full gap-4">
+          <p className="text-[20px]">Apartments</p>
+          <p className="text-[32px] font-medium">Sobha </p>
+          <p className="text-[18px] text-zinc-400">123</p>
         </div>
-        <div>
-          <div className="text-[16px] text-nowrap underline" onClick={onClick}> see configurations</div></div>
+        <div className="flex flex-col h-full justify-between w-fit">
+          <div>
+            <p className="text-[16px]">HSR Layout</p>
+            <p className="text-gray-400 text-[16px] text-nowrap">
+              {" "}
+              North Bangalore
+            </p>
+          </div>
+          <div>
+            {!expandedState && (
+              <div
+                className="text-[16px] text-nowrap underline text-zinc-400 cursor-pointer"
+                onClick={onClick}
+              >
+                {" "}
+                see configurations
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-    {expandedState && (
-      <div className="bg-white text-black">
-        <p> CONFIGURATIONS</p>
-        <p>APARTMENTS</p>
-        <div className="flex gap-1">
-          <div className="bg-gray-300">
-            1bhk
+      {expandedState && (
+        <div className="bg-white text-black px-8 pb-4">
+          <p className="text-gray-400 text-[18px]"> CONFIGURATIONS</p>
+          <div>
+            <p className="text-[20px] ">APARTMENTS</p>
+            <div className="flex gap-1">
+              <div className="bg-slate-200 rounded-md">
+                <div className="p-2 mr-4">
+                  <p className="font-medium text-[22px]">1 bhk</p>
+                  <p className="text-[22px]">100 sqft</p>
+                </div>
+              </div>
+              <div className="bg-slate-200 rounded-md">
+                <div className="p-2 mr-4">
+                  <p className="font-medium text-[22px]">1 bhk</p>
+                  <p className="text-[22px]">100 sqft</p>
+                </div>
+              </div>
+              <div className="bg-slate-200 rounded-md">
+                <div className="p-2 mr-4">
+                  <p className="font-medium text-[22px]">1 bhk</p>
+                  <p className="text-[22px]">100 sqft</p>
+                </div>
+              </div>
             </div>
-            <div className="bg-gray-300">
-            1bhk
-            </div>
-            <div className="bg-gray-300">
-            1bhk
+            <div className="flex justify-end">
+              <div
+                className="text-[16px] text-nowrap underline text-zinc-400"
+                onClick={onClick}
+              >
+                see less
+              </div>
             </div>
           </div>
-          <p>APARTMENTS</p>
-        <div className="flex gap-1">
-          <div className="bg-gray-300">
-            1bhk
-            </div>
-            <div className="bg-gray-300">
-            1bhk
-            </div>
-            <div className="bg-gray-300">
-            1bhk
-            </div>
-          </div>
-      </div>
-    )}
+        </div>
+      )}
     </div>
   );
 }
