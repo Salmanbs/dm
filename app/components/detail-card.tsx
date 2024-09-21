5bbcb4b44fda7a8e5f6f4fda247d1c0d75a2ab98
salman <salmanbinsalahudeen@gmail.com>
2024-09-21 20:24:51 +0530

    feat(footer): made responsove and style fine tuning

--- app/components/detail-card.tsx
+++ app/components/detail-card.tsx
@@ -3,7 +3,9 @@ import InfoCard from "./info-card";
 export default function DetailCard() {
   return (
     <div className="mt-3">
-      <p className="text-[20px] text-custom-gray-600">APARTMENTS</p>
+      <p className="text-[12px] md:text-[20px] text-custom-gray-600 mb-3">
+        APARTMENTS
+      </p>
       <div className="flex gap-1">
         <InfoCard />
         <InfoCard />
--- app/components/info-card.tsx
+++ app/components/info-card.tsx
@@ -2,8 +2,12 @@ export default function InfoCard() {
   return (
     <div className="bg-custom-gray-200 rounded-md">
       <div className="p-2 mr-4">
-        <p className="font-medium text-[22px] text-custom-gray-600">1 bhk</p>
-        <p className="text-[22px] text-custom-gray-500">100 sqft</p>
+        <p className="font-medium text-[16px] md:text-[22px] text-custom-gray-600">
+          1 bhk
+        </p>
+        <p className="text-[12px] md:text-[22px] text-custom-gray-500">
+          100 sqft
+        </p>
       </div>
     </div>
   );
